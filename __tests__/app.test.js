021cd806bd35f1bc2c3b7615945e426b4c35a9d5
aloan93 <a.loan93@gmail.com>
2023-10-02 14:38:37 +0100

    complete ex3 GET /api

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -37,3 +37,41 @@ describe("GET /api/topics", () => {
       });
   });
 });
+
+describe("GET /api", () => {
+  test("should return a 200 status code", () => {
+    return request(app).get("/api").expect(200);
+  });
+  test("should return an accurate JSON object", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        expect(typeof body.apis).toBe("string");
+        expect(typeof JSON.parse(body.apis)).toBe("object");
+      });
+  });
+  test("should return each endpoint with a key of 'description', as well as 'queries' and 'exampleResponse' when necessary", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        const parsedResult = JSON.parse(body.apis);
+        for (const api in parsedResult) {
+          expect(typeof parsedResult[api].description).toBe("string");
+          if (Object.keys(parsedResult[api]).length > 1) {
+            expect(Array.isArray(parsedResult[api].queries)).toBe(true);
+            expect(typeof parsedResult[api].exampleResponse).toBe("object");
+          }
+        }
+      });
+  });
+  test("should return a 404 status code and 'Not Found' when passed an incorrectly spelled api request", () => {
+    return request(app)
+      .get("/apo")
+      .expect(404)
+      .then(({ res }) => {
+        expect(res.statusMessage).toBe("Not Found");
+      });
+  });
+});
