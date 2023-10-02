88074df4c8aa38169f9eb1535432df8d848caa77
aloan93 <a.loan93@gmail.com>
2023-10-02 17:08:54 +0100

    complete ex4 GET-/api/articles/article_id

--- app.js
+++ app.js
@@ -8,6 +8,22 @@ app.get("/api/topics", getTopics);
 
 app.get("/api/articles/:article_id", getArticleById);
 
+// custom errors
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ message: err.message });
+  }
+  next(err);
+});
+
+// psql errors
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ message: "Invalid Data Type" });
+  }
+});
+
+// failsafe (500)
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(500).send("Internal Server Error");
--- models/articles.models.js
+++ models/articles.models.js
@@ -1,3 +1,15 @@
 const db = require("../db/connection");
 
-exports.fetchArticleById = () => {};
+exports.fetchArticleById = (article_id) => {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
+    .then((output) => {
+      if (output.rows < 1) {
+        return Promise.reject({ status: 404, message: "Article Not Found" });
+      } else {
+        const article = output.rows[0];
+        article.created_at = article.created_at.toString();
+        return article;
+      }
+    });
+};
